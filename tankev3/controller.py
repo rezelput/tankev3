5da43326bac843ddf6506d7d94998cee4b6a3ae8
Sofia <gn17o@yandex.ru>
2020-05-19 17:46:28 +0300

    Initial commit

---
+++ tankev3/controller.py
@@ -0,0 +1,24 @@
+from time import sleep
+from random import choice, randint
+
+from ev3dev2.motor import OUTPUT_B, OUTPUT_C, LargeMotor
+from ev3dev2.button import Button
+from ev3dev2.sensor.lego import InfraredSensor
+
+motors = [LargeMotor(adress) for adress in (OUTPUT_B,OUTPUT_C)]
+ir = InfraredSensor()
+print('Robot Starting')
+btn = Button()
+
+def start()
+for m in motors:
+    m.run_direct()
+
+def turn()
+power = choice([(1,-1),(-1,1)])
+t = randint(250, 750)
+for m, p in zip(motors, power):
+    m.run_timed(speed_sp = p*750, time_sp = t)
+    while any(m.state for m in motors):
+        sleep(0.1)
+
